af3d8680a12bff8f50255d002028e66e7c5e5237
UP2019511 <savvikopikovs@yahoo.com>
2024-12-17 00:53:36 +0000

    first commit
    lol

---
+++ src/mainComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MainComponent() {
+  return (
+    <div class="mainContainer1">
+        <h2 class="">Sava Opikovs</h2>
+        <img src='https://avatars.githubusercontent.com/u/59495444?v=4' class="profileimage"></img>
+    </div>
+  )
+}
